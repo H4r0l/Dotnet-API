531ad11affc2c2bf700c4a214cf2ce06a68466bf
Har0l <65699208+H4r0l@users.noreply.github.com>
2023-09-08 20:14:21 -0500

    fix: hot reload

--- Program.cs
+++ Program.cs
@@ -2,6 +2,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    if (hostingContext.HostingEnvironment.IsDevelopment())
+    {
+        config.AddUserSecrets<Program>();
+    }
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
